b138edbf9ba25676661a776283ff612c8ca250f5
Steven <easteven94700@gmail.com>
2022-01-25 18:30:41 +0100

    add function sign_message and verif_message + test sign_and_verif message corrected

--- p2p/p2p_client.py
+++ p2p/p2p_client.py
@@ -11,23 +11,19 @@ Options:
     --port_source=<int>  port source [default: 8000]
 """
 
-import sys
-import logging
-import threading
-from docopt import docopt
-from flask import Flask
-from flask import Response
-from flask import request
+import base64
+import hashlib
 import http.client
 import json
-
+import logging
 import os
-import hashlib
+import sys
+import threading
 
 import OpenSSL
+from docopt import docopt
+from flask import Flask, Response, request
 from OpenSSL import crypto
-import base64
-
 
 app = Flask(__name__)
 # app.debug = True
@@ -46,27 +42,27 @@ def send_certificate(certificate, target_ip, target_port, username):
     la raison associee.
     """
     # print("-- post function called --")
-    
+
     if os.path.exists(certificate):
         last_four_char = certificate[-4:]
-        if (last_four_char == ".pem"):
-            certificate_file = open(certificate,mode='r')
-            certificate_content = certificate_file.read()        
+        if last_four_char == ".pem":
+            certificate_file = open(certificate, mode="r")
+            certificate_content = certificate_file.read()
             certificate_file.close()
-        else : 
+        else:
             print("Error : File is not a certificate")
-            return -1,-1,-1
+            return -1, -1, -1
     else:
         print("Error : File does not exist")
-        return -1,-1,-1
-    
+        return -1, -1, -1
+
     try:
         conn = http.client.HTTPConnection(target_ip, target_port)
-        headers = {'Content-Type': 'application/json'}
-        dataToServer = {'username': username, 'clef_pub': certificate_content}
+        headers = {"Content-Type": "application/json"}
+        dataToServer = {"username": username, "clef_pub": certificate_content}
         jsonData = json.dumps(dataToServer)
-        
-        conn.request('POST', '/p2p_post_key', jsonData, headers)
+
+        conn.request("POST", "/p2p_post_key", jsonData, headers)
         response = conn.getresponse()
         msgReceived = response.read().decode()
         serverStatus = response.status
@@ -76,11 +72,74 @@ def send_certificate(certificate, target_ip, target_port, username):
         print("Failed to connect to server. Try again later.")
         return -1, -1, -1
 
-def sign_message(message,private_key,password)
-    return -1
 
-def verify_sign(message,signature,certificate)
-    return -1
+def sign_message(message, private_key, password):
+    """
+    Arguments : le message à signer (string)
+                la private key pour signer le message(path)
+                le password utiliser lors de la génération de la clé
+    -Lire le contenue de la private_key
+    -Charger la private key
+    -Signer le message avec la private key
+
+    Retourne : Le message signée (bytes) et la signature généré (bytes)
+                -1,-1 en cas d'erreur
+    """
+    if os.path.exists(private_key):
+        last_four_char = private_key[-4:]
+        if last_four_char == ".pem":
+            key_file = open(private_key, mode="r")
+            key_content = key_file.read()
+            key_file.close()
+        else:
+            print("Error : File is not a Private Key")
+            return -1, -1
+    else:
+        print("Error : File does not exist")
+        return -1, -1
+
+    pkey = crypto.load_privatekey(crypto.FILETYPE_PEM, key_content, password.encode())
+
+    bytes_message = str.encode(message)
+    signature = crypto.sign(pkey, bytes_message, "sha256")
+
+    return bytes_message, signature
+
+
+def verify_sign(bytes_message, signature, certificate):
+    """
+    Arguments : le message signé à vérifier (bytes)
+                lal signature du message(bytes)
+                le certificate pour vérifier la signature (path)
+    -Lire le contenue du certificate
+    -Charger le certificate
+    -Vérification de la signature
+
+    Retourne : -1,-1 en cas d'erreur
+                0,0 Avec si le message à été signer ou non
+    """
+    if os.path.exists(certificate):
+        last_four_char = certificate[-4:]
+        if last_four_char == ".pem":
+            certificate_file = open(certificate, mode="rb")
+            certificate_content = certificate_file.read()
+            certificate_file.close()
+        else:
+            print("Error : File is not a Certificate")
+            return -1, -1
+    else:
+        print("Error : File does not exist")
+        return -1, -1
+
+    cert = crypto.load_certificate(crypto.FILETYPE_PEM, certificate_content)
+
+    try:
+        OpenSSL.crypto.verify(cert, signature, bytes_message, "sha256")
+        print("Message is signed")
+    except:
+        print("Message is not signed")
+    return 0, 0
+
 
 def send_message(message, target_ip, target_port, username):
     """
@@ -97,12 +156,12 @@ def send_message(message, target_ip, target_port, username):
     # print("-- post function called --")
     try:
         conn = http.client.HTTPConnection(target_ip, target_port)
-        headers = {'Content-Type': 'application/json'}
-        dataToServer = {'username': username, 'text': message}
+        headers = {"Content-Type": "application/json"}
+        dataToServer = {"username": username, "text": message}
         jsonData = json.dumps(dataToServer)
         print(jsonData)
-        
-        conn.request('POST', '/p2p_post', jsonData, headers)
+
+        conn.request("POST", "/p2p_post", jsonData, headers)
         response = conn.getresponse()
         msgReceived = response.read().decode()
         serverStatus = response.status
@@ -112,67 +171,66 @@ def send_message(message, target_ip, target_port, username):
         print("Failed to connect to server. Try again later.")
         return -1, -1, -1
 
+
 def compose_message(target_ip, target_port, user):
     """
     Arguments : adresse IP du destinataire, le port du destinataire,
     username du destinataire
-    Demande a l'utilisateur un message en input pour envoyer au 
+    Demande a l'utilisateur un message en input pour envoyer au
     destinataire.
     """
-    while(True):
-        text_input = input('>> ')
-        (data_send, 
-        server_status,
-        server_reason) = send_message(text_input, 
-                                      target_ip,
-                                      target_port,
-                                      user)
+    while True:
+        text_input = input(">> ")
+        (data_send, server_status, server_reason) = send_message(
+            text_input, target_ip, target_port, user
+        )
 
 
 def server(ipaddress, local_port, user):
     """
-    Creer un serveur avec une adresse ip et un port passes en 
+    Creer un serveur avec une adresse ip et un port passes en
     arguments
     """
     app.run(host=ipaddress, port=local_port)
 
 
-@app.route('/p2p_post', methods=['POST']) 
+@app.route("/p2p_post", methods=["POST"])
 def p2p_post():
     """
     Recoit le message d'un client et l'affiche dans la console
     """
     data = request.get_json()
-    text = data.get('text', '')
-    ip = data.get('ip', '')
-    clef_publique = data.get('clef_pub', '')
-    print("<< {} : {}".format(user, text,clef_publique))
+    text = data.get("text", "")
+    ip = data.get("ip", "")
+    clef_publique = data.get("clef_pub", "")
+    print("<< {} : {}".format(user, text, clef_publique))
     return data
 
-@app.route('/p2p_post_key', methods=['POST']) 
+
+@app.route("/p2p_post_key", methods=["POST"])
 def p2p_post_key():
     """
     Recoit le message d'un client et l'affiche dans la console
     """
     data = request.get_json()
-    clef_publique = data.get('clef_pub', '')
+    clef_publique = data.get("clef_pub", "")
     print("<< {} : {}".format(user, clef_publique))
     return data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ARGS = docopt(__doc__)
 
-    user = ARGS['--buddy']
-    target_ip = ARGS['--ip']
-    target_port = ARGS['--port_dest']
-    ip = ARGS['--ip']
-    source_port = ARGS['--port_source']
+    user = ARGS["--buddy"]
+    target_ip = ARGS["--ip"]
+    target_port = ARGS["--port_dest"]
+    ip = ARGS["--ip"]
+    source_port = ARGS["--port_source"]
     try:
-        thread1 = threading.Thread(target=compose_message, 
-                                    args=(target_ip, target_port, user))
-        thread2 = threading.Thread(target=server, 
-                                    args=(ip, source_port, user))
+        thread1 = threading.Thread(
+            target=compose_message, args=(target_ip, target_port, user)
+        )
+        thread2 = threading.Thread(target=server, args=(ip, source_port, user))
         thread1.start()
         thread2.start()
         thread1.join()
--- p2p/test_p2p_client.py
+++ p2p/test_p2p_client.py
@@ -1,25 +1,26 @@
-import unittest
-import requests
+import http.client
+import io
+import json
 import shlex
 import subprocess
+import sys
 import time
-import json
-import http.client
+import unittest
+
+import requests
+
 import p2p_client
 
-import io
-import sys
-
-
-#Pour generer une cle privé :
+# Pour generer une cle privé :
 # $ openssl genrsa -aes128 -passout pass:<password> -out private.pem 4096
 
-#Pour generer une cle publique a partir de la cle privé
+# Pour generer une cle publique a partir de la cle privé
 # $ openssl rsa -in private.pem -passin pass:<password> -pubout -out public.pem
 
-#Pour generer un certificat a partir de la cle privé
+# Pour generer un certificat a partir de la cle privé
 # $ openssl req -new -x509 -sha256 -key private.pem -out cert.pem -days <int>
 # NE PAS OUBLIER DE MODIFIER LES PATH DANS LES VARIABLES
+
 
 class TestP2PClient(unittest.TestCase):
 
@@ -33,23 +34,21 @@ class TestP2PClient(unittest.TestCase):
     buddyUsr1 = "Ground"
     buddyUsr2 = "Air"
     msgFromGround = "Hello Air, it's a message from Ground!"
-    msgJsonGround = {'username': buddyUsr2, 'text': msgFromGround}
+    msgJsonGround = {"username": buddyUsr2, "text": msgFromGround}
     msgFromAir = "Hi Ground, it's a message from Air!"
-    msgJsonAir = {'username': buddyUsr1, 'text': msgFromAir}
-    
-    certificate_from_air="/home/steven/cert.pem" #path du certificat.pem
-    privatekey = "/home/steven/private.pem"
-    password = "azertyuiop"
-    message_to_sign = "Message à signer"
-    certificate2 ="home/steven/cert2.pem"
+    msgJsonAir = {"username": buddyUsr1, "text": msgFromAir}
 
+    certificate_from_air = "/home/steven/cert.pem"  # path du certificat.pem
+    private_key = "/home/steven/private.pem"
+    password = "azertyuiop"
+    certificate2 = "/home/steven/cert2.pem"
 
     def setUp(self):
         # Launch User1 Ground terminal
         cmdGround = "python3 p2p_client.py --buddy={} --port_dest={}\
-                    --port_source={}".format(self.buddyUsr2,
-                                             self.portUser1, 
-                                             self.portUser2)
+                    --port_source={}".format(
+            self.buddyUsr2, self.portUser1, self.portUser2
+        )
         argsGround = shlex.split(cmdGround)
         # launch command as a subprocess
         self.user1Subprocess = subprocess.Popen(argsGround)
@@ -57,180 +56,165 @@ class TestP2PClient(unittest.TestCase):
 
         # Launch User2 Air terminal
         # cmdAir = "python3 p2p_client.py --port_dest={} --port_source={} \
-        #         --buddy={}".format(self.port_dest, 
-                                     # self.port_source,
-                                     # self.buddyUsr2)
+        #         --buddy={}".format(self.port_dest,
+        # self.port_source,
+        # self.buddyUsr2)
         # argsAir = shlex.split(cmdAir)
         # self.user2Subprocess = subprocess.Popen(argsAir)
         # time.sleep(3)
 
-
     def test_send_message(self):
         # Test response and connection to server Ground with client
         # Air with a string msg
-        (dataSend,
-            serverStatus, 
-            serverReason) = p2p_client.send_message(self.msgFromAir,
-                                                    self.ip,
-                                                    self.portUser2, 
-                                                    self.buddyUsr1)
+        (dataSend, serverStatus, serverReason) = p2p_client.send_message(
+            self.msgFromAir, self.ip, self.portUser2, self.buddyUsr1
+        )
         dataSend = json.loads(dataSend)
-        self.assertEqual(dataSend['text'], self.msgJsonAir['text'])
-        self.assertEqual(dataSend['username'], 
-                         self.msgJsonAir['username'])
+        self.assertEqual(dataSend["text"], self.msgJsonAir["text"])
+        self.assertEqual(dataSend["username"], self.msgJsonAir["username"])
         self.assertEqual(dataSend, self.msgJsonAir)
         self.assertEqual(serverStatus, 200)
         self.assertEqual(serverReason, "OK")
 
         # Test response and connection to server Ground with client
         # Air with an int msg
-        (dataSend, 
-            serverStatus, 
-            serverReason) = p2p_client.send_message(123456789, 
-                                                    self.ip, 
-                                                    self.portUser2, 
-                                                    self.buddyUsr1)
+        (dataSend, serverStatus, serverReason) = p2p_client.send_message(
+            123456789, self.ip, self.portUser2, self.buddyUsr1
+        )
         dataSend = json.loads(dataSend)
-        self.assertNotEqual(dataSend['text'], self.msgJsonAir['text'])
-        self.assertEqual(dataSend['text'], 123456789)
-        self.assertEqual(dataSend['username'], 
-                         self.msgJsonAir['username'])
+        self.assertNotEqual(dataSend["text"], self.msgJsonAir["text"])
+        self.assertEqual(dataSend["text"], 123456789)
+        self.assertEqual(dataSend["username"], self.msgJsonAir["username"])
         self.assertNotEqual(dataSend, self.msgJsonAir)
         self.assertEqual(serverStatus, 200)
         self.assertEqual(serverReason, "OK")
 
-        # Test response and connection to server Ground with client 
+        # Test response and connection to server Ground with client
         # Air with an empty string
-        (dataSend, 
-            serverStatus, 
-            serverReason) = p2p_client.send_message("", self.ip, 
-                                                    self.portUser2, 
-                                                    self.buddyUsr1)
+        (dataSend, serverStatus, serverReason) = p2p_client.send_message(
+            "", self.ip, self.portUser2, self.buddyUsr1
+        )
         dataSend = json.loads(dataSend)
-        self.assertEqual(dataSend['text'], "")
-        self.assertEqual(dataSend['username'], 
-                            self.msgJsonAir['username'])
+        self.assertEqual(dataSend["text"], "")
+        self.assertEqual(dataSend["username"], self.msgJsonAir["username"])
         self.assertEqual(serverStatus, 200)
         self.assertEqual(serverReason, "OK")
 
         # Test response and connection to server Ground with client
         # Air with a dictionnary
-        (dataSend, 
-            serverStatus, 
-            serverReason) = p2p_client.send_message(self.msgJsonAir, 
-                                                    self.ip, 
-                                                    self.portUser2, 
-                                                    self.buddyUsr1)
+        (dataSend, serverStatus, serverReason) = p2p_client.send_message(
+            self.msgJsonAir, self.ip, self.portUser2, self.buddyUsr1
+        )
         dataSend = json.loads(dataSend)
-        dico = {'username' : self.buddyUsr1,
-                'text' : {'username': self.buddyUsr1, 
-                            'text': self.msgFromAir}}
+        dico = {
+            "username": self.buddyUsr1,
+            "text": {"username": self.buddyUsr1, "text": self.msgFromAir},
+        }
         dico = json.dumps(dico)
         dico = json.loads(dico)
-        self.assertEqual(dataSend['text'], dico['text'])
-        self.assertNotEqual(dataSend['text'], self.msgJsonAir['text'])
-        self.assertEqual(dataSend['username'], 
-                            self.msgJsonAir['username'])
-        self.assertEqual(dataSend['text']['username'], 
-                            self.msgJsonAir['username'])
+        self.assertEqual(dataSend["text"], dico["text"])
+        self.assertNotEqual(dataSend["text"], self.msgJsonAir["text"])
+        self.assertEqual(dataSend["username"], self.msgJsonAir["username"])
+        self.assertEqual(dataSend["text"]["username"], self.msgJsonAir["username"])
         self.assertEqual(serverStatus, 200)
         self.assertEqual(serverReason, "OK")
 
         # Test response and connection to server Ground with client
-         # Air with a list
+        # Air with a list
         listMsg = ["bonjour", "comment tu vas?", 4, "byebye", 8]
-        (dataSend, 
-            serverStatus, 
-            serverReason) = p2p_client.send_message(listMsg, 
-                                                    self.ip, 
-                                                    self.portUser2, 
-                                                    self.buddyUsr1)
+        (dataSend, serverStatus, serverReason) = p2p_client.send_message(
+            listMsg, self.ip, self.portUser2, self.buddyUsr1
+        )
         dataSend = json.loads(dataSend)
-        self.assertEqual(dataSend['text'], listMsg)
-        self.assertEqual(dataSend['text'][0], listMsg[0])
-        self.assertNotEqual(dataSend['text'][2], listMsg[0])
-        self.assertEqual(dataSend['text'][2], listMsg[2])
-        self.assertNotEqual(dataSend['text'], 
-                            self.msgJsonAir['text'])
-        self.assertEqual(dataSend['username'], 
-                        self.msgJsonAir['username'])
+        self.assertEqual(dataSend["text"], listMsg)
+        self.assertEqual(dataSend["text"][0], listMsg[0])
+        self.assertNotEqual(dataSend["text"][2], listMsg[0])
+        self.assertEqual(dataSend["text"][2], listMsg[2])
+        self.assertNotEqual(dataSend["text"], self.msgJsonAir["text"])
+        self.assertEqual(dataSend["username"], self.msgJsonAir["username"])
         self.assertEqual(serverStatus, 200)
         self.assertEqual(serverReason, "OK")
-
-
 
     def tearDown(self):
         print("killing subprocess user_server")
         self.user1Subprocess.kill()
         self.user1Subprocess.wait()
 
-
-    #Test de la fonction send_public_key()
+    # Test de la fonction send_public_key()
     def test_certificate_key(self):
 
-        #Test du fichier envoyé
+        # Test du fichier envoyé
         get_printed_output = io.StringIO()
         sys.stdout = get_printed_output
-        self.assertEqual(p2p_client.send_certificate("file_not_found","0.0.0.0","8000","8080"),(-1,-1,-1))
+        self.assertEqual(
+            p2p_client.send_certificate("file_not_found", "0.0.0.0", "8000", "8080"),
+            (-1, -1, -1),
+        )
         sys.stdout = sys.__stdout__
-        self.assertEqual("Error : File does not exist\n",get_printed_output.getvalue())
+        self.assertEqual("Error : File does not exist\n", get_printed_output.getvalue())
 
         get_printed_output = io.StringIO()
         sys.stdout = get_printed_output
-        self.assertEqual(p2p_client.send_certificate("p2p_client.py","0.0.0.0","8000","8080"),(-1,-1,-1))
+        self.assertEqual(
+            p2p_client.send_certificate("p2p_client.py", "0.0.0.0", "8000", "8080"),
+            (-1, -1, -1),
+        )
         sys.stdout = sys.__stdout__
-        self.assertEqual("Error : File is not a certificate\n",get_printed_output.getvalue())
+        self.assertEqual(
+            "Error : File is not a certificate\n", get_printed_output.getvalue()
+        )
 
-        (dataSend,
-            serverStatus, 
-            serverReason) = p2p_client.send_certificate(self.certificate_from_air,
-                                                        self.ip,
-                                                        self.portUser2, 
-                                                        self.buddyUsr1)
+        (dataSend, serverStatus, serverReason) = p2p_client.send_certificate(
+            self.certificate_from_air, self.ip, self.portUser2, self.buddyUsr1
+        )
         dataSend = json.loads(dataSend)
 
-        #test contenu du fichier 
-        self.assertEqual(len(dataSend['clef_pub']), 1428)
-        self.assertEqual(dataSend['clef_pub'][:28], '-----BEGIN CERTIFICATE-----\n')
-        self.assertEqual((dataSend['clef_pub'][-26:]), '-----END CERTIFICATE-----\n')
+        # test contenu du fichier
+        self.assertEqual(len(dataSend["clef_pub"]), 1428)
+        self.assertEqual(dataSend["clef_pub"][:28], "-----BEGIN CERTIFICATE-----\n")
+        self.assertEqual((dataSend["clef_pub"][-26:]), "-----END CERTIFICATE-----\n")
 
-        
-
-    def test_sign&verif_message(self)
-        #Test message signé et verification de la signature
-        (signed_message,sign) = sign_message(message_to_sign,private_key,password)
-        
+    def test_sign_and_verif_message(self):
+        # Test message signé et verification de la signature
+        (signed_message, sign) = p2p_client.sign_message(
+            "Message a signer", self.private_key, self.password
+        )
         get_printed_output = io.StringIO()
         sys.stdout = get_printed_output
-        verify_sign(signed_message,sign,certificate)
+        p2p_client.verify_sign(signed_message, sign, self.certificate_from_air)
         sys.stdout = sys.__stdout__
-        self.assertEqual("Message is signed\n",get_printed_output.getvalue())
+        self.assertEqual("Message is signed\n", get_printed_output.getvalue())
 
-        #Test Vérification d'un message non signé et d'une signature random
+        # Test Vérification d'un message non signé et d'une signature random
         get_printed_output = io.StringIO()
         sys.stdout = get_printed_output
-        verify_sign(message_to_sign,sign,certificate)
+        p2p_client.verify_sign(
+            str.encode("Message non signée"), sign, self.certificate_from_air
+        )
         sys.stdout = sys.__stdout__
-        self.assertEqual("Message is not signed\n",get_printed_output.getvalue())
+        self.assertEqual("Message is not signed\n", get_printed_output.getvalue())
 
-        #Test message signé puis modification du message et verification signature
-        (signed_message,sign) = sign_message(message_to_sign,private_key,password)
-        signed_message += "Modification du message" 
+        # Test message signé puis modification du message et verification signature
+        (signed_message, sign) = p2p_client.sign_message(
+            "Nouveau message à signer", self.private_key, self.password
+        )
+        signed_message += str.encode("Modification du message")
         get_printed_output = io.StringIO()
         sys.stdout = get_printed_output
-        verify_sign(signed_message,sign,certificate)
+        p2p_client.verify_sign(signed_message, sign, self.certificate_from_air)
         sys.stdout = sys.__stdout__
-        self.assertEqual("Message is not signed\n",get_printed_output.getvalue())
+        self.assertEqual("Message is not signed\n", get_printed_output.getvalue())
 
-        #Test message signé et Vérification avec un autre certificat
-        (signed_message,sign) = sign_message(message_to_sign,private_key,password)
-        signed_message += "Modification du message" 
+        # Test message signé et Vérification avec un autre certificat
+        (signed_message, sign) = p2p_client.sign_message(
+            "Dernier message a signer", self.private_key, self.password
+        )
         get_printed_output = io.StringIO()
         sys.stdout = get_printed_output
-        verify_sign(signed_message,sign,cert2)
+        p2p_client.verify_sign(signed_message, sign, self.certificate2)
         sys.stdout = sys.__stdout__
-        self.assertEqual("Message is not signed\n",get_printed_output.getvalue())
+        self.assertEqual("Message is not signed\n", get_printed_output.getvalue())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
