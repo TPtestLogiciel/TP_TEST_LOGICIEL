75c08ef3afca5fe4aeed60560465390d768cccca
Maxime Lebreton <maxime.lebreton@etu.upmc.fr>
2022-01-25 16:25:26 +0100

    Ajout de la branche Docker

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#Les commandes comprises dans ce fichier seront executées lorsque le container sera run sur le runner !"
+#!/bin/sh -l
+#/bin/echo "Welcome !"
+
+
+cd workflow/
+find ./ -type f -name "test*.py" | xargs -n 1 python3
+
+#ls
+
+#find ./client/ -type f -name "test*.py" | xargs -n 1 python3
+#find ./server/ -type f -name "test*.py" | xargs -n 1 python3
+#find ./p2p/ -type f -name "test*.py" | xargs -n 1 python3
+
+echo "Hello $1"
+time=$(date)
+echo "::set-output name=time::$time"
+
