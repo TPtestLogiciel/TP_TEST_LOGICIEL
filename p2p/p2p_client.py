dae00d587a42ba3954898fcebf1f457b8ec3cf36
lolacostul <lolapnnt@yahoo.com>
2022-01-25 22:59:40 +0100

    Ajout d'une fonction GET au serveur pour avoir l'ip et le port d'un utilisateur

--- p2p/p2p_client.py
+++ p2p/p2p_client.py
@@ -1,14 +1,11 @@
 """p2p_client
 
 Usage:
-    p2p_client.py --buddy=<buddy> [--ip=<ip>] --port_dest=<int> --port_source=<int>
+    p2p_client.py --buddy=<buddy>
 
 Options:
     -h --help  Show this screen for help
     --buddy=<buddy>  buddy username
-    --ip=<ip>  ip used [default: 0.0.0.0]
-    --port_dest=<int>  port destinataire
-    --port_source=<int>  port source
 """
 
 import http.client
@@ -40,7 +37,7 @@ def send_message(message, target_ip, target_port, username):
         server_reason = server_response.reason
         return msg_received, server_status, server_reason
     except ConnectionRefusedError:
-        print("Failed to connect to server. Try again later.")
+        print("Failed to connect to {}. Try again later.".format(username))
         return -1, -1, -1
 
 
@@ -51,6 +48,16 @@ def compose_message(target_ip, target_port, user):
     while True:
         text_input = input(">> ")
         send_message(text_input, target_ip, target_port, user)
+
+def get_ip_port(server_ip, server_port, user):
+    conn_server = http.client.HTTPConnection(server_ip, server_port)
+    conn_server.request("GET", "/get_ip_port/{}".format(user))
+
+    server_response = conn_server.getresponse()
+    msg_received = server_response.read().decode()
+    server_status = server_response.status
+    server_reason = server_response.reason
+    return msg_received, server_status, server_reason
 
 
 def server(ip_address, local_port, user):
@@ -74,18 +81,23 @@ def p2p_post():
 if __name__ == "__main__":
     ARGS = docopt(__doc__)
 
+    server_ip = "0.0.0.0"
+    server_port = 8000
     user = ARGS["--buddy"]
-    target_port = ARGS["--port_dest"]
-    source_port = ARGS["--port_source"]
-    ip_address = ARGS["--ip"]
-    try:
-        thread1 = threading.Thread(
-            target=compose_message, args=(ip_address, target_port, user)
-        )
-        thread2 = threading.Thread(target=server, args=(ip_address, source_port, user))
-        thread1.start()
-        thread2.start()
-        thread1.join()
-        thread2.join()
-    except KeyboardInterrupt:
-        print("Press Ctrl+C to remove server part")
+    (msg_received, status, reason) = get_ip_port(server_ip, server_port, user)
+    print(msg_received, status, reason)
+
+    # source_port = 8001
+    # target_port = msg_received["port"]
+    # ip_address = msg_received["ip_address"]
+    # try:
+    #     thread1 = threading.Thread(
+    #         target=compose_message, args=(ip_address, target_port, user)
+    #     )
+    #     thread2 = threading.Thread(target=server, args=(ip_address, source_port, user))
+    #     thread1.start()
+    #     thread2.start()
+    #     thread1.join()
+    #     thread2.join()
+    # except KeyboardInterrupt:
+    #     print("Press Ctrl+C to remove server part")
