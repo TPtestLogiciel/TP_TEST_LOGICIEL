1888a3969b3a1102931f4de404b76cdac3a5ce62
Maxime Lebreton <maxime.lebreton@etu.upmc.fr>
2022-01-27 11:38:45 +0100

    execution des tests Python, pour chaque repertoire

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,28 @@ myvar2=$(pwd)
 echo "::set-output name=myvar2::$myvar2"
 
 python3 --version
-find ./ -type f -name "test*.py" | xargs -n 1 python3 || true
+
+
+
+#for d in */ ; do
+#    cd $d
+#    find ./ -type f -name "test*.py" | xargs -n 1 python3 || true
+#done
+
+#https://stackoverflow.com/questions/10446186/cd-into-directory-in-while-loop-doesnt-work/10446617
+
+ls -1 | while read d
+do 
+    test -d "$d" || continue
+    echo " "
+    echo " "
+    echo "dossier" $d ":"
+    echo " "
+    echo " "
+    (cd $d ; find ./ -type f -name "test*.py" | xargs -n 1 python3 || true)
+done
+
+
+
 rm -rf ./*
-ls
+
